43d232dc91c8d0af38ec30df8d4eb71b61b17ed1
CuongSuperCode <nvcuongbk94@gmail.com>
2022-10-09 12:01:01 +0700

    change fetchJwt

--- src/api/fetchJwt.js
+++ src/api/fetchJwt.js
@@ -1,9 +1,8 @@
 export const fetchJwt = () => {
   let jwt = {}
-  jwt = localStorage.getItem('jwt_super_admin')
+  jwt = localStorage.getItem('jwt')
   // value: {"role":"super_admin","value":"404"}
 
-  // jwt = localStorage.getItem('jwt_region_manager')
   // value: {"role":"region_manager","value":"123"}
 
   if (jwt) {
